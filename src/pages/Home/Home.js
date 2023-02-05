84cad45e87c4c6b528609d36948ab9109da9fb9d
Matin Mousavi <msvmatin5049@gmail.com>
2023-02-05 22:37:59 +0330

    Footer Section was added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,7 @@ import AboutUs from '../../Components/AboutUs/AboutUs'
 import PopularCourses from '../../Components/PopularCourses/PopularCourses'
 import PreSellCourses from '../../Components/PreSellCourses/PreSellCourses'
 import LastArticles from '../../Components/LastArticles/LastArticles'
+import Footer from '../../Components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -20,6 +21,7 @@ const Home = () => {
     <PopularCourses />
     <PreSellCourses />
     <LastArticles />
+    <Footer />
     </>
   )
 }
