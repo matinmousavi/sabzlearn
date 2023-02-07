65d6c89c708842ca7cc0bbc33320bc569dd890b6
Matin Mousavi <msvmatin5049@gmail.com>
2023-02-08 00:50:29 +0330

    Login and Register pages created

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import CourseInfo from "./pages/CourseInfo/CourseInfo";
 import Category from "./pages/Category/Category";
 import Home from "./pages/Home/Home";
 import Courses from "./pages/Courses/Courses";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 const routes = [
   { path: "/", element: <Home /> },
@@ -10,6 +12,8 @@ const routes = [
   { path: "/category-info/:courseName", element: <Category /> },
   { path: "/article-info/:articleName", element: <ArticleInfo /> },
   { path: "/courses", element: <Courses /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
 ];
 
 export default routes;
